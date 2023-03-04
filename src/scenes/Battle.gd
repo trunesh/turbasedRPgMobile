a90aebe4813b9b2ee7b8ae52d6b0cc967add5f78
trunesh loke <trunesh.loke123@gmail.com>
2023-03-04 19:25:04 +0530

    taking double damage lol..need to check

---
+++ src/scenes/Battle.gd
@@ -0,0 +1,32 @@
+extends Node
+
+onready var hpLabel=$Enemy/HPLabel
+onready var enemy = $Enemy
+onready var playerStats = $PlayerStats
+onready var battleActionButtons = $UI/BattleActionButtons
+
+func _ready():
+	start_player_turn()
+	
+func start_enemy_turn():
+	battleActionButtons.hide()
+	if enemy!=null:
+		enemy.attack(playerStats)
+		yield(enemy,"end_turn")
+	start_player_turn()
+
+func start_player_turn():
+	battleActionButtons.show()
+	playerStats.ap = playerStats.max_ap
+	yield(playerStats,"end_turn")
+	start_enemy_turn()
+ 
+func _on_Button_pressed():
+	if enemy!=null:
+		enemy.take_damage(4)
+		playerStats.ap-=1 
+
+
+func _on_Enemy_on_death(): 
+	battleActionButtons.hide()
+	enemy=null
---
+++ src/scenes/Enemy.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+var hp = 25 setget set_hp
+var target = null
+
+
+onready var hpLabel = $HPLabel
+
+onready var animationPlayer = $AnimationPlayer
+
+signal on_death
+signal end_turn
+
+func set_hp(new_hp):
+	hp = new_hp
+	hpLabel.text= str(hp) + "hp"
+	
+#	if hp <=0:
+#		emit_signal("on_death")
+#		queue_free()
+#	else:
+#		animationPlayer.play("Shake") 
+#		yield(animationPlayer, "animation_finished")
+#		animationPlayer.play("Attack")
+#		yield(animationPlayer,"animation_finished")
+#		var battle = get_tree().current_scene
+#		var player = battle.find_node("PlayerStats")
+#		player.hp-=3
+
+func attack(target)-> void:
+	yield(get_tree().create_timer(0.4),"timeout")
+	animationPlayer.play("Attack")
+	self.target =target
+	
+	yield(animationPlayer,"animation_finished")
+	target.hp-=4
+	
+	self.target  = null
+	emit_signal("end_turn")
+	
+func take_damage(amount):
+	self.hp-=amount
+	if is_dead():
+		emit_signal("on_death")
+		queue_free()
+	else:
+		animationPlayer.play("Shake")
+func is_dead():
+	return hp <=0
+
+func deal_damage():
+	self.target.hp -=4
+	print("deal damage")
+	
+	
